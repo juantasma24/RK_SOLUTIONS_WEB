ec351a5ceda486dece5543ba2ba1c7bc3bcec55c
juantasma24 <juantasma24@users.noreply.github.com>
2026-05-14 11:49:50 +0200

    Se hizo la migración a Next.js del avance

--- rk-web-next/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "standalone", // Genera un servidor Node.js optimizado para producción
-};
-
-export default nextConfig;
-
-
